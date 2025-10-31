aa2a59a4878ff543e74d4cbcccb645ea12bcb51f
zerekovua123-web <zerekovua123@gmail.com>
2025-10-31 15:30:56 +0300

    Create 8.1.py

---
+++ 8.1.py
@@ -0,0 +1,15 @@
+for i in range(5):
+  for j in range(5):
+    print(5. end=' ')
+print()
+
+for i in range(5):
+  for j in range(1. 11):
+    print(j. end=' ')
+print()
+
+start = 41
+for i in range(4):
+  for j in range(10):
+    print(start + i * 10 + j. end=' ')
+print()
